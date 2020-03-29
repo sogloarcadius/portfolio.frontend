fba752c8488b44565d83e1dc2b13b266747ab2b4
sogloarcadius <rtsoglo@gmail.com>
2020-03-29 22:29:53 +0200

    wip : setup pages templates

---
+++ website/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand" href="#">
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="@/assets/img/profile.jpeg" alt />
+        </span>
+      </a>
+      <div class="collapse navbar-collapse" id="">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+              <router-link class="nav-link"  :to="{ name: 'home' }"><a>About</a></router-link>
+          </li>
+          <li class="nav-item">
+              <router-link class="nav-link"  :to="{ name: 'projects' }"><a>Portfolio</a></router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="home">
+
+     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 class="mb-0">Arcadius
+          <span class="text-primary">SOGLO</span>
+        </h1>
+        <div class="subheading mb-5">Rennes, France ·
+          <a href="#">sogloarcadius@yahoo.fr</a>
+        </div>
+        <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+        <div class="social-icons">
+          <a target="_blank" href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a target="_blank" href="#">
+            <i class="fab fa-github"></i>
+          </a>
+          <a target="_blank" href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a target="_blank" href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+      </div>
+    </section>
     
   </div>
 </template>
